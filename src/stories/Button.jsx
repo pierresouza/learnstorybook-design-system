ecba2cd0f937da71567fdd9ac86f0e91c8e4c864
pierresouza <pierre.s3@hotmail.com>
2025-02-19 12:16:52 -0300

    fix: fixing imports

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-import "./ButtonExample.css";
+import "./Button.css";
 
 /** Primary UI component for user interaction */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,6 +1,6 @@
 import { fn } from "@storybook/test";
 
-import { Button } from "./ButtonExample";
+import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
