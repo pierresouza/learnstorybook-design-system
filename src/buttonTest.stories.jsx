66bf3704ca79654726905ae7172d24ca07a162cf
pierresouza <pierre.s3@hotmail.com>
2025-02-25 15:47:19 -0300

    chore: change structure from variants

--- src/buttonTest.stories.jsx
+++ src/buttonTest.stories.jsx
@@ -27,96 +27,72 @@ export default {
     },
     disabled: {
       options: ["boolean"], // Estado desabilitado
-      constrol: { type: "select" },
     },
   },
 };
 
+const Template = (args) => <ButtonTest {...args} />;
+
 // Exemplo padrão
-export const Default = {
-  args: {
-    children: "Default Button",
-    onClick: action("clicked"), // Registra cliques no Storybook
-    type: "filled",
-    size: "lg",
+export const Default = Template.bind({});
+Default.args = {
+  children: "Default Button",
+  onClick: action("clicked"), // Registra cliques no Storybook
+  type: "filled",
+  size: "lg",
+};
+
+Default.parameters = {
+  docs: {
+    source: {
+      code: "Your code snippet goes here.",
+      language: "jsx",
+      type: "auto",
+    },
   },
 };
 
 // Botão com ícone à esquerda
-export const WithIconLeft = {
-  args: {
-    children: "Button with Icon",
-    icon: <span>⭐</span>, // Ícone de exemplo
-    iconPosition: "left",
-    type: "filled",
-    size: "lg",
-  },
-};
-
-// Botão com ícone à direita
-export const WithIconRight = {
-  args: {
-    children: "Button with Icon",
-    icon: <span>⭐</span>, // Ícone de exemplo
-    iconPosition: "right",
-    type: "filled",
-    size: "lg",
-  },
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  children: "Button with Icon",
+  icon: <span>⭐</span>, // Ícone de exemplo
+  iconPosition: "left",
+  type: "filled",
+  size: "lg",
 };
 
 // Botão desabilitado
-export const Disabled = {
-  args: {
-    disabled: true,
-    children: "Disabled Button",
-    type: "filled",
-    size: "lg",
-  },
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  children: "Disabled Button",
+  type: "filled",
+  size: "lg",
 };
 
 // Botão como link
-export const AsLink = {
-  args: {
-    children: "Link Button",
-    action: "link",
-    to: "#", // URL de exemplo
-    type: "filled",
-    size: "lg",
-  },
+export const AsLink = Template.bind({});
+AsLink.args = {
+  children: "Link Button",
+  action: "link",
+  to: "#", // URL de exemplo
+  type: "filled",
+  size: "lg",
 };
 
 // Botão pequeno
-export const SmallSize = {
-  args: {
-    children: "Small Button",
-    type: "filled",
-    size: "sm",
-  },
+export const SmallSize = Template.bind({});
+SmallSize.args = {
+  children: "Small Button",
+  type: "filled",
+  size: "sm",
 };
 
 // Botão grande
-export const LargeSize = {
-  args: {
-    children: "Large Button",
-    type: "filled",
-    size: "lg",
-  },
-};
-
-// Botão com tipo "outlined"
-export const Outlined = {
-  args: {
-    children: "Outlined Button",
-    type: "outlined",
-    size: "lg",
-  },
-};
-
-// Botão com tipo "text"
-export const Text = {
-  args: {
-    children: "Text Button",
-    type: "text",
-    size: "lg",
-  },
+export const LargeSize = Template.bind({});
+LargeSize.args = {
+  children: "Large Button",
+  type: "filled",
+  size: "lg",
 };
